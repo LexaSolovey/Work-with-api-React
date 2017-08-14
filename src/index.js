66da4f0d5057f6a466993a5bec4f57bed6acce52
LexaSolovey <xoxach5@gmail.com>
2017-08-15 02:39:58 +0300

    React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Route, BrowserRouter} from 'react-router-dom';
+import {Route, BrowserRouter, HashRouter} from 'react-router-dom';
 import './components/Home/index.css';
 import Home from './components/Home/index';
 import Movie from './components/Movie/Movie';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <div>
       <Route exact path="/" component={Home} />
       <Route path="/:id" component={Movie} />
     </div>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root'));
 registerServiceWorker();
